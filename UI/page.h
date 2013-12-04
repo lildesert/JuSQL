21e10a4e8a748e249fae04790100075b9924ce72
jeff <jeff@R2D2>
2013-12-04 10:22:34 +0100

    définition de la strcuture pour contenir une Page

---
+++ UI/page.h
@@ -0,0 +1,13 @@
+
+#ifndef PAGE_H
+#define	PAGE_H
+
+// Définition d'une structure Page
+// contenant un ensemble de 512 éléments / bits
+
+struct Page {
+    char e[512];
+};
+
+#endif	/* PAGE_H */
+
